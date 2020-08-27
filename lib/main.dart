d4a1a0724661aff48551a23235ff04c0eeee8008
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-27 15:23:39 -0500

    adding text input widgets

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,14 @@ class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
       id: '1',
-      title: 'New Keyboard',
+      title: 'Mechanical Keyboard',
       amount: 87.99,
       date: DateTime.now(),
     ),
     Transaction(
       id: '2',
-      title: 'New Headphones',
-      amount: 143.23,
+      title: 'Sony Headphones',
+      amount: 43.23,
       date: DateTime.now(),
     )
   ];
@@ -37,7 +37,7 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        // mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
@@ -46,6 +46,28 @@ class MyHomePage extends StatelessWidget {
               color: Colors.blue,
               child: Text('Chart'),
               elevation: 5,
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                  ),
+                ],
+              ),
             ),
           ),
           Column(
