22e4045a0182f4eac6e2afc7dd655c97d16854b7
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-27 14:54:17 -0500

    Using String Interpolation

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class MyHomePage extends StatelessWidget {
                           border: Border.all(color: Colors.purple, width: 2)),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tx.amount.toString(),
+                        '\$${tx.amount}',
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
@@ -67,11 +67,15 @@ class MyHomePage extends StatelessWidget {
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(tx.title),
                         Text(
-                          tx.date.toString(),
-                        )
+                          tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(tx.date.toString(),
+                            style: TextStyle(color: Colors.grey))
                       ],
                     )
                   ],
