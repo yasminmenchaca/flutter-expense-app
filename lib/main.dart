d4b2e5c5d206b3d799e795adde561a0935ff0add
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-28 14:07:02 -0500

    adding images to the app

--- lib/main.dart
+++ lib/main.dart
@@ -43,56 +43,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: '1',
-      title: 'Mechanical Keyboard',
-      amount: 87.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '2',
-      title: 'Item 1',
-      amount: 43.23,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '3',
-      title: 'Item 2',
-      amount: 43.23,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '4',
-      title: 'Headphones',
-      amount: 3.23,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '5',
-      title: 'Item 3',
-      amount: 33.23,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '6',
-      title: 'Item 4',
-      amount: 43.23,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '7',
-      title: 'Item 5',
-      amount: 43.23,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: '8',
-      title: 'Item 6',
-      amount: 43.23,
-      date: DateTime.now(),
-    )
-  ];
+  final List<Transaction> _userTransactions = [];
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
