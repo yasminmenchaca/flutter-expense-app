3171aa376cbe30989ebf942ceb1f9b1c20d1a1fb
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-08-28 10:46:42 -0500

    showing a modal bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/new_transaction.dart';
+import './models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,18 +15,101 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   // String titleInput;
   // String amountInput;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: '1',
+      title: 'Mechanical Keyboard',
+      amount: 87.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'Item 1',
+      amount: 43.23,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '3',
+      title: 'Item 2',
+      amount: 43.23,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '4',
+      title: 'Headphones',
+      amount: 3.23,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '5',
+      title: 'Item 3',
+      amount: 33.23,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '6',
+      title: 'Item 4',
+      amount: 43.23,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '7',
+      title: 'Item 5',
+      amount: 43.23,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '8',
+      title: 'Item 6',
+      amount: 43.23,
+      date: DateTime.now(),
+    )
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('See, Spend, Save'),
-        actions: [IconButton(icon: Icon(Icons.add), onPressed: () {})],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -39,14 +124,14 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            UserTransaction()
+            TransactionList(_userTransactions),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
