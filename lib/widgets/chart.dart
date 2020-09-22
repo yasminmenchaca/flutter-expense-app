ca7539110ab84c15216fbd61cb8a805f83f089f8
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-22 12:29:00 -0500

    creating bars for chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -20,7 +20,7 @@ class Chart extends StatelessWidget {
       }
       print(DateFormat.E().format(weekDay));
       print(totalSum);
-      return {'day': DateFormat.E().format(weekDay), 'amount': totalSum};
+      return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'amount': totalSum};
     });
   }
 
@@ -31,7 +31,9 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
