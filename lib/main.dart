de12b10e13697993bb776fa4b4ddd2cab49af068
Yasmin Menchaca <yasmin.a.menchaca@gmail.com>
2020-09-23 10:46:30 -0500

    added button theme

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
                     fontFamily: 'OpenSans',
                     fontWeight: FontWeight.bold,
                     fontSize: 18),
+                button: TextStyle(color: Colors.white),
               ),
           appBarTheme: AppBarTheme(
             textTheme: ThemeData.light().textTheme.copyWith(
